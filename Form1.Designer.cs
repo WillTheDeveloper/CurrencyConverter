5aa5bfab9eb80ab0b2739aef0e1a08a4e526010b
WillTheDeveloper <twitchifygamers@gmail.com>
2021-12-12 18:44:40 +0000

    Created plus and minus buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,9 @@
             this.currencies = new CurrencyCalculator.currencies();
             this.currenciesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.currenciesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             ((System.ComponentModel.ISupportInitialize)(this.currencies)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.currenciesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.currenciesBindingSource1)).BeginInit();
@@ -150,12 +153,32 @@
             this.currenciesBindingSource1.DataMember = "currencies";
             this.currenciesBindingSource1.DataSource = this.currenciesBindingSource;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.darkmode);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
@@ -188,6 +211,9 @@
         private System.Windows.Forms.BindingSource currenciesBindingSource1;
         private System.Windows.Forms.BindingSource currenciesBindingSource;
         public currencies currencies;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HelpProvider helpProvider1;
     }
 }
 
